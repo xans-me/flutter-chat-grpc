f1d548a928a5bf6e16794a7abcb97f467d527b04
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2020-12-28 13:07:15 +0700

    feat: global activate of plugin_proto

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter-grpc-client-example
+
+### In this tutorial, we take a look at the basic of building a gRPC client in Flutter/Dart.  We use an existing Go gRPC service to do this.  
+
+### To build this application, you will need the latest version of the flutter SDK.  
---
+++ lib/service/chat_service.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+class ChatService {
+  User user = User();
+  static BroadcastClient client;
+
+  ChatService(String name) {
+    user
+      ..clearName()
+      ..name = name
+      ..clearId()
+      ..id = sha256.convert(utf8.encode(user.name)).toString();
+
+    client = BroadcastClient(
+      ClientChannel(
+        "10.0.2.2",
+        port: 8080,
+        options: ChannelOptions(
+          credentials: ChannelCredentials.insecure(),
+        ),
+      ),
+    );
+  }
+
+  Future<Close> sendMessage(String body) async {
+    return client.broadcastMessage(
+      Message()
+        ..id = user.id
+        ..content = body
+        ..timestamp = DateTime.now().toIso8601String(),
+    );
+  }
+
+  Stream<Message> recieveMessage() async* {
+    Connect connect = Connect()
+      ..user = user
+      ..active = true;
+
+    await for (var msg in client.createStream(connect)) {
+      yield msg;
+    }
+  }
+}
