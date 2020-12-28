dc24d114bd7728c39351d9897d12a02e90d637cd
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2020-12-29 03:47:19 +0700

    feat: add chat service

---
+++ lib/service/chat_service.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:flutter_chat_grpc/protobuff/service.pb.dart';
+import 'package:flutter_chat_grpc/protobuff/service.pbgrpc.dart';
+import 'package:grpc/grpc.dart';
+
+class ChatService {
+  User user = User();
+
+  // init broadcast client which actual use to communicate with the server
+  // static to always to same instance
+  static BroadcastClient client;
+
+  // constructor for ChatService class
+  ChatService(String username) {
+    // setup user
+    user
+      ..clearName()
+      ..name = username
+      ..clearId()
+      ..id = sha256.convert(utf8.encode(user.name)).toString();
+
+    // setup a client
+    client = BroadcastClient(
+      ClientChannel(
+        "192.168.100.13",
+        port: 8080,
+        options: ChannelOptions(
+          credentials: ChannelCredentials.insecure(),
+        ),
+      ),
+    );
+  }
+
+  // method async to send message
+  Future<Close> sendMessage(String body) async {
+    return client.broadcastMessage(
+      Message()
+        ..id = user.id
+        ..content = body
+        ..timestamp = DateTime.now().toIso8601String()
+        ..user = user,
+    );
+  }
+
+  // method async to receive message
+  Stream<Message> receiveMessage() async* {
+    Connect connect = Connect()
+      ..user = user
+      ..active = true;
+
+    // await for loop to get all various messages from calling client
+    await for (var msg in client.createStream(connect)) {
+      yield msg;
+    }
+  }
+}
